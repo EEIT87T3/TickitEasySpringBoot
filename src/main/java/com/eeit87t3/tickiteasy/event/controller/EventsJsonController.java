6e1904a479faea0a23b2b0e5da8c046c5c8cdd68
chuan13 <chuan02421@gmail.com>
2024-10-06 23:28:13 +0800

    feat: 後台活動功能完成部分。
    feat: 後台活動功能完成部分。
    feat: 後台模板使用白色版 Logo。

--- src/main/java/com/eeit87t3/tickiteasy/event/controller/EventsJsonController.java
+++ src/main/java/com/eeit87t3/tickiteasy/event/controller/EventsJsonController.java
@@ -1,13 +1,20 @@
 package com.eeit87t3.tickiteasy.event.controller;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,22 +33,14 @@ public class EventsJsonController {
 	@Autowired
 	private EventsService eventsService;
 
-
-	@GetMapping
-	public List<EventsEntity> findByCategoryOrTagString(
+	@GetMapping()
+	public Page<EventsEntity> findByDynamic(
+			@RequestParam(value = "p", defaultValue = "1") Integer pageNumber,
 			@RequestParam(value = "category-string", required = false) String categoryString,
-			@RequestParam(value = "tag-string", required = false) String tagString) {
-		if (categoryString != null && tagString == null) {
-			return eventsService.findByCategoryString(categoryString);
-		} else if (categoryString == null && tagString != null) {
-			return eventsService.findByTagString(tagString);
-		} else {
-			EventsEntity eventsEntity = new EventsEntity();
-			eventsEntity.setEventName("暫時訊息：else");
-			List<EventsEntity> list = new ArrayList<>();
-			list.add(eventsEntity);
-			return list;
-		}
+			@RequestParam(value = "tag-string", required = false) String tagString,
+			@RequestParam(value = "searching-time", required = false) LocalDateTime searchingTime
+			) {
+		return eventsService.findByDynamic(pageNumber, categoryString, tagString, searchingTime);
 	}
 	
 	@GetMapping("/{eventID}")
@@ -49,9 +48,45 @@ public class EventsJsonController {
 		return eventsService.findById(eventID);
 	}
 	
+	@PostMapping
+	public ResponseEntity<?> create(@ModelAttribute EventsDTO createEventsDTO) {
+		String validateCreateInput = eventsService.validateCreateInput(createEventsDTO);
+		if ("輸入正確！".equals(validateCreateInput)) {
+			return new ResponseEntity<EventsEntity>(eventsService.create(createEventsDTO), HttpStatus.CREATED);
+		} else {
+			return new ResponseEntity<String>(validateCreateInput, HttpStatus.BAD_REQUEST);
+		}
+	}
+	
 	@PutMapping("/{eventID}")
-	public EventsEntity edit(@ModelAttribute EventsDTO editEventsDTO) {
-		// 輸入驗證：422 Unprocessable Entity
-		return eventsService.edit(editEventsDTO);
+	public ResponseEntity<?> edit(@ModelAttribute EventsDTO editEventsDTO) {
+		String validateEditInput = eventsService.validateEditInput(editEventsDTO);
+		if ("輸入正確！".equals(validateEditInput)) {
+			return new ResponseEntity<EventsEntity>(eventsService.edit(editEventsDTO), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<String>(validateEditInput, HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@PutMapping("/{eventID}/edit-status")
+	public ResponseEntity<?> editStatus(@PathVariable Integer eventID,
+			@RequestBody Map<String, Object> requestBody) {
+		Short editStatus = ((Integer) requestBody.get("editStatus")).shortValue();
+		String validateEditStatus = eventsService.validateEditStatus(eventID, editStatus);
+		if ("輸入正確！".equals(validateEditStatus)) {
+			return new ResponseEntity<EventsEntity>(eventsService.editStatus(eventID, editStatus), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<String>(validateEditStatus, HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@DeleteMapping("/{eventID}")
+	public ResponseEntity<?> delete(@PathVariable Integer eventID) {
+		String validateDeleteInput = eventsService.validateDeleteInput(eventID);
+		if ("輸入正確！".equals(validateDeleteInput)) {
+			return new ResponseEntity<Boolean>(eventsService.delete(eventID), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<String>(validateDeleteInput, HttpStatus.BAD_REQUEST);
+		}
 	}
 }
--- src/main/java/com/eeit87t3/tickiteasy/event/service/EventsService.java
+++ src/main/java/com/eeit87t3/tickiteasy/event/service/EventsService.java
@@ -1,12 +1,17 @@
 package com.eeit87t3.tickiteasy.event.service;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.eeit87t3.tickiteasy.categoryandtag.entity.CategoryEntity;
@@ -16,6 +21,7 @@ import com.eeit87t3.tickiteasy.categoryandtag.service.TagService;
 import com.eeit87t3.tickiteasy.event.dto.EventsDTO;
 import com.eeit87t3.tickiteasy.event.entity.EventsEntity;
 import com.eeit87t3.tickiteasy.event.repository.EventsRepo;
+import com.eeit87t3.tickiteasy.event.repository.EventsSpecification;
 import com.eeit87t3.tickiteasy.image.ImageDirectory;
 import com.eeit87t3.tickiteasy.image.ImageUtil;
 
@@ -84,33 +90,168 @@ public class EventsService {
 	}
 	
 	
+	public Page<EventsEntity> findByDynamic(Integer pageNumber, String categoryString, String tagString, LocalDateTime searchingTime) {
+		Specification<EventsEntity> specification = Specification.where(
+				EventsSpecification.hasCategoryString(categoryString)
+				.and(EventsSpecification.hasTagString(tagString))
+				.and(EventsSpecification.hasSerchingTime(searchingTime))
+				);
+		Pageable pageable = PageRequest.of(pageNumber - 1, 10, Direction.ASC, "eventID");  // （第幾頁（從 0 起算）, 一頁幾筆, 排序方向, 排序依據欄位）
+		return eventsFindingService.findBySpecificationAndPageable(specification, pageable);
+	}
+	
+	
+	// 新增
+	
+	/**
+	 * 新增活動的輸入驗證。
+	 * 
+	 * @param createEventsDTO
+	 * @return 驗證結果：若正確，回傳「輸入正確！」；若錯誤，會包含錯誤訊息。
+	 */
+	public String validateCreateInput(EventsDTO createEventsDTO) {
+
+		// 活動名稱
+		if (createEventsDTO.getEventName() != null && !createEventsDTO.getEventName().isBlank()) {  // 未輸入活動名稱
+			if (eventsRepo.findByEventName(createEventsDTO.getEventName()) != null) {
+				return "輸入錯誤：此活動名稱已經存在。";
+			}
+		} else {
+			return "輸入錯誤：未輸入活動名稱。";
+		}
+		
+		// 活動主圖
+		// （無限制）
+		
+		// 活動類別
+		if (createEventsDTO.getCategoryString() != null && !createEventsDTO.getCategoryString().isBlank()) {  // 有輸入活動類別
+			if (categoryService.findByCategoryString(createEventsDTO.getCategoryString()) == null) {
+				return "輸入錯誤：活動類別不存在。";
+			}
+		} else {  // 未輸入活動分類
+			return "輸入錯誤：未輸入活動類別。";
+		}
+		
+		// 活動標籤
+		if (createEventsDTO.getTagString() != null && !createEventsDTO.getTagString().isBlank()) {  // 有輸入活動標籤
+			if (tagService.findByTagString(createEventsDTO.getTagString()) == null) {
+				return "輸入錯誤：活動標籤不存在。";
+			}
+		}
+		
+		// 活動介紹
+		// （無限制）
+		
+		return "輸入正確！";
+	}
+	
+	/**
+	 * 新增活動。
+	 * 
+	 * @param createEventsDTO：新增內容。
+	 * @return 新增後的 EventsEntity。
+	 */
+	@Transactional
+	public EventsEntity create(EventsDTO createEventsDTO) {
+		EventsEntity eventsEntity = new EventsEntity();
+		
+		// 活動名稱
+		eventsEntity.setEventName(createEventsDTO.getEventName());
+		
+		// 活動類別
+		if (createEventsDTO.getCategoryString() != null && !createEventsDTO.getCategoryString().isBlank()) {  // 有輸入活動類別
+			CategoryEntity categoryEntity = categoryService.findByCategoryString(createEventsDTO.getCategoryString());
+			if (categoryEntity != null) {					
+				eventsEntity.setEventCategory(categoryEntity);
+			}
+		}
+		
+		// 活動標籤
+		if (createEventsDTO.getTagString() != null && !createEventsDTO.getTagString().isBlank()) {  // 有輸入活動標籤
+			TagEntity tagEntity = tagService.findByTagString(createEventsDTO.getTagString());
+			if (tagEntity != null) {
+				eventsEntity.setEventTag(tagEntity);
+			}
+		}
+		
+		// 活動介紹
+		if (createEventsDTO.getEventDesc() != null && !createEventsDTO.getEventDesc().isBlank()) {
+			eventsEntity.setEventDesc(createEventsDTO.getEventDesc());
+		}
+		
+		eventsRepo.save(eventsEntity);  // 先 persist、取得活動編號
+		// 活動主圖
+		if (createEventsDTO.getEventPicFile() != null && createEventsDTO.getEventPicFile().getSize() != 0) {  // 有替換活動主圖
+			String baseName = eventsEntity.getEventID().toString() + "_" + UUID.randomUUID().toString();
+			try {
+				String pathString = imageUtil.saveImage(ImageDirectory.EVENT, createEventsDTO.getEventPicFile(), baseName);
+				eventsEntity.setEventPic(pathString);
+			} catch (IllegalStateException | IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return eventsEntity;
+	}
+	
+	
 	// 修改
 	
-	public String validateEdit(EventsDTO editEventsDTO) {
-		Optional<EventsEntity> optional = eventsRepo.findById(editEventsDTO.getEventID());
-		if (optional.isEmpty()) {
-			return "輸入錯誤：這筆活動不存在。";
+	/**
+	 * 編輯活動的輸入驗證。
+	 * 
+	 * @param editEventsDTO：編輯內容。
+	 * @return 驗證結果：若正確，回傳「輸入正確！」；若錯誤，會包含錯誤訊息。
+	 */
+	public String validateEditInput(EventsDTO editEventsDTO) {
+		// 活動編號
+		EventsEntity eventsEntity = eventsFindingService.findById(editEventsDTO.getEventID());
+		if (eventsEntity == null) {
+			return "輸入錯誤：此筆活動不存在。";
 		} else {
-			EventsEntity eventsEntity = optional.get();
-			switch (eventsEntity.getStatus()) {
-				case 0:
-					if (editEventsDTO.getStatusString() != null && editEventsDTO.getStatusString() != "listing") {
-						return "輸入錯誤：「未上架」狀態只能修改為「已上架」狀態。";
-					}
-					return "未完成驗證。";
-				case 1:
-					if (editEventsDTO.getStatusString() != null && editEventsDTO.getStatusString() != "draft") {
-						return "輸入錯誤：「已上架」狀態只能修改為「未上架」狀態。";
-					}
-					return "未完成驗證。";
-				case 2:
-					if (editEventsDTO.getStatusString() != null) {
-						return "輸入錯誤：「已啟售」狀態不可修改狀態。";
-					}
-					return "未完成驗證。";
-				default:
-					return "輸入錯誤：狀態錯誤。";
+			
+			// 狀態
+			if (editEventsDTO.getStatusString() != null && !editEventsDTO.getStatusString().isBlank()) {  // 有修改狀態
+				return "輸入錯誤：無法在此修改狀態。";
 			}
+			
+			// 活動名稱
+			if (editEventsDTO.getEventName() != null && !editEventsDTO.getEventName().isBlank()) {  // 有修改活動名稱
+				switch (eventsEntity.getStatus()) {
+					case 0:  // 原本為「未上架」
+					case 1:  // 原本為「已上架」
+						if (eventsRepo.findByEventName(editEventsDTO.getEventName()) != null) {
+							return "輸入錯誤：此活動名稱已經存在。";
+						}
+						break;
+					case 2:  // 原本為「已啟售」
+						return "輸入錯誤：「已啟售」狀態不可修改活動名稱。";
+				}
+			}
+			
+			// 活動主圖
+			// （無限制）
+			
+			// 活動類別
+			if (editEventsDTO.getCategoryString() != null && !editEventsDTO.getCategoryString().isBlank()) {  // 有輸入活動類別
+				if (categoryService.findByCategoryString(editEventsDTO.getCategoryString()) == null) {
+					return "輸入錯誤：活動類別不存在。";
+				}
+			} else {  // 未輸入活動分類
+				return "輸入錯誤：未輸入活動類別。";
+			}
+			
+			// 活動標籤
+			if (editEventsDTO.getTagString() != null && !editEventsDTO.getTagString().isBlank()) {  // 有輸入活動標籤
+				if (tagService.findByTagString(editEventsDTO.getTagString()) == null) {
+					return "輸入錯誤：活動標籤不存在。";
+				}
+			}
+			
+			// 活動介紹
+			// （無限制）
+			
+			return "輸入正確！";
 		}
 	}
 	
@@ -122,11 +263,11 @@ public class EventsService {
 	 */
 	@Transactional
 	public EventsEntity edit(EventsDTO editEventsDTO) {
-		Optional<EventsEntity> optional = eventsRepo.findById(editEventsDTO.getEventID());
-		if (optional.isPresent()) {
-			EventsEntity eventsEntity = optional.get();
+		EventsEntity eventsEntity = eventsFindingService.findById(editEventsDTO.getEventID());
+		if (eventsEntity != null) {
 			
-			if (editEventsDTO.getStatusString() != null) {
+			// 狀態			
+			if (editEventsDTO.getStatusString() != null && !editEventsDTO.getStatusString().isBlank()) {  // 有修改狀態
 				switch (editEventsDTO.getStatusString()) {
 					case "draft":
 						eventsEntity.setStatus((short) 0);
@@ -136,10 +277,14 @@ public class EventsService {
 						break;
 				}
 			}
-			if (editEventsDTO.getEventName() != null) {
+			
+			// 活動名稱
+			if (editEventsDTO.getEventName() != null && !editEventsDTO.getEventName().isBlank()) {  // 有修改活動名稱
 				eventsEntity.setEventName(editEventsDTO.getEventName());
 			}
-			if (editEventsDTO.getEventPicFile() != null && editEventsDTO.getEventPicFile().getSize() != 0) {
+			
+			// 活動主圖
+			if (editEventsDTO.getEventPicFile() != null && editEventsDTO.getEventPicFile().getSize() != 0) {  // 有替換活動主圖
 				String baseName = eventsEntity.getEventID().toString() + "_" + UUID.randomUUID().toString();
 				try {
 					String newPathString = imageUtil.saveImage(ImageDirectory.EVENT, editEventsDTO.getEventPicFile(), baseName);
@@ -149,25 +294,129 @@ public class EventsService {
 					e.printStackTrace();
 				}
 			}
-			if (editEventsDTO.getCategoryString() != null) {
+			
+			// 活動類別
+			if (editEventsDTO.getCategoryString() != null && !editEventsDTO.getCategoryString().isBlank()) {  // 有輸入活動類別
 				CategoryEntity categoryEntity = categoryService.findByCategoryString(editEventsDTO.getCategoryString());
 				if (categoryEntity != null) {					
 					eventsEntity.setEventCategory(categoryEntity);
 				}
 			}
-			if (editEventsDTO.getTagString() != null) {
+			
+			// 活動標籤
+			if (editEventsDTO.getTagString() != null && !editEventsDTO.getTagString().isBlank()) {  // 有輸入活動標籤
 				TagEntity tagEntity = tagService.findByTagString(editEventsDTO.getTagString());
 				if (tagEntity != null) {
 					eventsEntity.setEventTag(tagEntity);
 				}
 			}
-			if (editEventsDTO.getEventDesc() != null) {
+			
+			// 活動介紹
+			if (editEventsDTO.getEventDesc() != null && !editEventsDTO.getEventDesc().isBlank()) {
 				eventsEntity.setEventDesc(editEventsDTO.getEventDesc());
 			}
 			
 			return eventsEntity;
 		} else {
 			return null;
+		}
+	}
+	
+	
+	/**
+	 * 編輯活動狀態的輸入驗證。
+	 * 
+	 * @param eventID
+	 * @param editStatus：要改為的狀態值。
+	 * @return 驗證結果：若正確，回傳「輸入正確！」；若錯誤，會包含錯誤訊息。
+	 */
+	public String validateEditStatus(Integer eventID, Short editStatus) {
+		// 活動編號
+		EventsEntity eventsEntity = eventsFindingService.findById(eventID);
+		if (eventsEntity == null) {
+			return "輸入錯誤：此筆活動不存在。";
+		} else {
+			// 狀態
+			switch (eventsEntity.getStatus()) {
+				case 0:  // 原本為「未上架」
+					if (editStatus != 1) {
+						return "輸入錯誤：「未上架」狀態只能修改為「已上架」狀態。";
+					}
+					break;
+				case 1:  // 原本為「已上架」
+					if (editStatus != 0) {
+						return "輸入錯誤：「已上架」狀態只能修改為「未上架」狀態。";
+					}
+					break;
+				case 2:  // 原本為「已啟售」
+					return "輸入錯誤：「已啟售」狀態不可修改狀態。";
+				default:
+					return "輸入錯誤：狀態錯誤。";
+			}
+		}
+		return "輸入正確！";
+	}
+	
+	/**
+	 * 編輯活動狀態。
+	 * 
+	 * @param eventID
+	 * @param editStatus：要改為的狀態值。
+	 * @return
+	 */
+	@Transactional
+	public EventsEntity editStatus(Integer eventID, Short editStatus) {
+		EventsEntity eventsEntity = eventsFindingService.findById(eventID);
+		if (eventsEntity != null) {
+			eventsEntity.setStatus(editStatus);
+		}
+		return eventsEntity;
+	}
+	
+	
+	// 刪除
+	
+	/**
+	 * 刪除活動的輸入驗證。
+	 * 
+	 * @param eventID：要刪除的活動之活動編號。
+	 * @return 驗證結果：若正確，回傳「輸入正確！」；若錯誤，會包含錯誤訊息。
+	 */
+	public String validateDeleteInput(Integer eventID) {
+		EventsEntity eventsEntity = eventsFindingService.findById(eventID);
+		if (eventsEntity == null) {
+			return "輸入錯誤：此筆活動不存在。";
+		} else {
+			if (eventsEntity.getStatus() != 0) {
+				return "輸入錯誤：此筆活動為「已上架」或「已啟售」狀態，不可刪除。";
+			} else {
+				return "輸入正確！";
+			}
+		}
+	}
+	
+	/**
+	 * 刪除活動。
+	 * 
+	 * @param eventID：要刪除的活動之活動編號。
+	 * @return 刪除執行結果。
+	 */
+	public Boolean delete(Integer eventID) {
+		EventsEntity eventsEntity = eventsFindingService.findById(eventID);
+		
+		// 刪除圖片
+		try {
+			imageUtil.deleteImage(eventsEntity.getEventPic());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		eventsRepo.delete(eventsEntity);
+		EventsEntity result = eventsFindingService.findById(eventID);
+		if (result == null) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 }
