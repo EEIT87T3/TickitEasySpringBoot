07ff73b4cf3167305c25f85b8eeaad45b0eafae2
weifang1030 <weifang.chang1030@gmail.com>
2024-10-07 08:13:52 +0800

    PostController更新註解+刪除無用程式碼

--- src/main/java/com/eeit87t3/tickiteasy/post/controller/PostController.java
+++ src/main/java/com/eeit87t3/tickiteasy/post/controller/PostController.java
@@ -56,28 +56,32 @@ public class PostController {
 	@Autowired
 	private CommentRepo commentRepo;
 	
+///////////////////////////////////////     頁面跳轉    //////////////////////////////////////
+	//所有貼文頁面
 	 @GetMapping("/posts")
 	    public String showPostsPage(Model model) {
-	        return "post/postList"; // 返回你的 Thymeleaf 模板，確保模板的路徑正確
+	        return "post/postList"; 
 	    }
+	 //更新貼文頁面
+	 @GetMapping("/GetUpdate")
+	 public String showUpdateForm() {
+		 return "post/editPost";  // 返回 insertpost.html
+	 }
 	 
-	//新增貼文
+	//新增貼文頁面 *未完成*
 	@GetMapping("/create")
 	    public String showInsertForm() {
-	        return "post/insertpost";  // 返回 insertpost.html
+	        return "post/insertpost";  
 	    }
-	//更新貼文頁面
-	@GetMapping("/GetUpdate")
-	public String showUpdateForm() {
-		return "post/editPost";  // 返回 insertpost.html
-	}
-	//更新貼文
-//		@GetMapping("/update/{postID}")
-//		    public String showUpdateForm(@PathVariable Integer postID,Model model) {
-//			model.addAttribute("postID", postID);
-//	   
-//			return "post/editPost";  
-//		    }
+	//留言頁面
+	 @GetMapping("/comments/{postID}")
+	    public String showCommentsPage(@PathVariable Integer postID,Model model) {
+		 	model.addAttribute("postID", postID);
+	        return "post/postComment"; // 返回你的 Thymeleaf 模板，確保模板的路徑正確
+	    }
+	//////////////////////////////////////////////////////////////////////////////////////////
+
+	//取得更新貼文內容
 	@GetMapping("/update")
 	public ResponseEntity<PostEntity> editPost(@RequestParam("postID") Integer postID) {
 	    PostEntity post = postService.findById(postID);
@@ -86,41 +90,14 @@ public class PostController {
 	    }
 	    return ResponseEntity.ok(post);
 	}
-
-
-
-		
 	//取得所有貼文
-// 	@GetMapping
-// 	public String findAll(Model model) {
-// 		List<PostEntity> posts = postService.findAll();
-// 		model.addAttribute("posts",posts);
-// 		
-// 		return "post/PostList";	
-//		
-//	}
 	@GetMapping
 	public ResponseEntity<List<PostEntity>> getAllPosts() {
 	    List<PostEntity> posts = postService.findAll(); // 獲取所有貼文
 	    return ResponseEntity.ok(posts);
 	}
- 	//根據貼文ID取得單筆貼文
-// 	@GetMapping("/{postID}")
-// 	public String findById(
-// 			@ModelAttribute PostEntity post,
-// 			@RequestParam("postID")Integer postID,
-// 			Model model) {
-// 		
-// 		PostEntity findPost = postService.findById(postID);
-// 	
-// 		model.addAttribute("post",findPost);
-//	    
-// 		List<CommentEntity> findComments = commentService.findById(postID);
-// 		
-// 		model.addAttribute("comment", findComments);
-// 		return "post/post";	
-// 		
-// 	}
+
+	//根據id取得單一貼文
 	@GetMapping("/{postID}")
 	public ResponseEntity<PostEntity> getPostById(@PathVariable("postID") Integer postID) {
 	    try {
@@ -131,33 +108,9 @@ public class PostController {
 	    }
 	}
 
- 	@PostMapping("/getUpdatePost")
- 	public String getUpdatePost(@RequestParam("postID")Integer postID,Model model) {
- 		
-
-		PostEntity foundPost = postService.findById(postID); 
-		model.addAttribute("post", foundPost);
- 		
- 		return "post/UpdatePost";	
- 		
- 	}
- 	//根據貼文主題取得多筆貼文
-// 	@GetMapping("/findByTheme")
-// 	public String findByTheme(
-// 			@RequestParam("category") CategoryEntity category,
-// 			Model model) {
-// 		
-// 		List<PostEntity> posts = postService.findByCategory(category);
-// 		model.addAttribute("posts",posts);
-// 		if(category == null) {
-// 			return "redirect:/post/findAll";
-// 		}
-// 		return "post/PostList";	
-// 		
-// 	}
- 	//方法2
+ 	//根據類別取得多筆貼文 *未完成*
  	@GetMapping("/category")
- 	public ResponseEntity<List<PostEntity>> getEventsByCategory(@RequestParam("categoryID") Integer categoryID) {
+ 	public ResponseEntity<List<PostEntity>> getPostsByCategory(@RequestParam("categoryID") Integer categoryID) {
  	    // 根據 categoryID 查詢 CategoryEntity
  	    Optional<CategoryEntity> optionalCategory = categoryRepo.findById(categoryID);
  	    
@@ -170,6 +123,8 @@ public class PostController {
  	    // 如果類別不存在，回傳 404 Not Found
  	    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
  	}
+ 	
+ 	//根據貼文id取得多筆留言
  	@GetMapping("/comment")
  	public ResponseEntity<List<CommentEntity>> findCommentByPostId(@RequestParam("postID") Integer postID) {
  
@@ -184,39 +139,8 @@ public class PostController {
  		// 如果類別不存在，回傳 404 Not Found
  		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
  	}
- 	 @GetMapping("/comments/{postID}")
-	    public String showCommentsPage(@PathVariable Integer postID,Model model) {
- 		 	model.addAttribute("postID", postID);
-	        return "post/postComment"; // 返回你的 Thymeleaf 模板，確保模板的路徑正確
-	    }
-// 	//根據貼文ID取得單筆貼文-留言
-// 	@GetMapping("/comment")
-// 	public String findCommentByPostId(
-// 			@ModelAttribute PostEntity post,
-// 			@RequestParam("postID")Integer postID,
-// 			Model model) {
-// 		
-// 		
-// 		List<CommentEntity> findComments = commentService.findById(postID);
-// 		
-// 		model.addAttribute("comment", findComments);
-// 		return "post/post";	
-// 		
-// 	}
-
  	
-// 	//根據搜尋內容取得多筆貼文
-// 	@GetMapping("/findByEnter")
-// 	public String findByEnter(
-// 			@RequestParam("enter") String enter,
-// 			Model model) {
-// 		
-// 		List<PostEntity> posts = postService.findByEnter(enter);
-// 		model.addAttribute("posts",posts);
-// 		
-// 		return "post/PostList";	
-// 		
-// 	}
+
  	//新增單筆貼文
  	@PostMapping("/create")
  	public ResponseEntity<String> createPost(@RequestParam("postTitle") String title, 
@@ -271,14 +195,92 @@ public class PostController {
  		return "post/postComment";	
  	}
  	
+ 	//取得類別列表
  	@GetMapping("/categories")
  	public ResponseEntity<List<CategoryEntity>> getAllCategories() {
  	    List<CategoryEntity> categories = categoryService.findPostCategoryList();
  	    return ResponseEntity.ok(categories);
  	}
+ 	//取得標籤列表
  	@GetMapping("/tags")
  	public ResponseEntity<List<TagEntity>> getAllTags() {
  	    List<TagEntity> tags = tagService.findPostTagList();
  	    return ResponseEntity.ok(tags);
  	}
+ 	
+	//更新貼文
+//	@GetMapping("/update/{postID}")
+//	    public String showUpdateForm(@PathVariable Integer postID,Model model) {
+//		model.addAttribute("postID", postID);
+//   
+//		return "post/editPost";  
+//	    }
+// 	//根據貼文ID取得單筆貼文-留言
+// 	@GetMapping("/comment")
+// 	public String findCommentByPostId(
+// 			@ModelAttribute PostEntity post,
+// 			@RequestParam("postID")Integer postID,
+// 			Model model) {
+// 		
+// 		
+// 		List<CommentEntity> findComments = commentService.findById(postID);
+// 		
+// 		model.addAttribute("comment", findComments);
+// 		return "post/post";	
+// 		
+// 	}
+
+ 	
+// 	//根據搜尋內容取得多筆貼文
+// 	@GetMapping("/findByEnter")
+// 	public String findByEnter(
+// 			@RequestParam("enter") String enter,
+// 			Model model) {
+// 		
+// 		List<PostEntity> posts = postService.findByEnter(enter);
+// 		model.addAttribute("posts",posts);
+// 		
+// 		return "post/PostList";	
+// 		
+// 	}
+ 	//根據貼文主題取得多筆貼文
+// 	@GetMapping("/findByTheme")
+// 	public String findByTheme(
+// 			@RequestParam("category") CategoryEntity category,
+// 			Model model) {
+// 		
+// 		List<PostEntity> posts = postService.findByCategory(category);
+// 		model.addAttribute("posts",posts);
+// 		if(category == null) {
+// 			return "redirect:/post/findAll";
+// 		}
+// 		return "post/PostList";	
+// 		
+// 	}
+ 	//根據貼文ID取得單筆貼文
+// 	@GetMapping("/{postID}")
+// 	public String findById(
+// 			@ModelAttribute PostEntity post,
+// 			@RequestParam("postID")Integer postID,
+// 			Model model) {
+// 		
+// 		PostEntity findPost = postService.findById(postID);
+// 	
+// 		model.addAttribute("post",findPost);
+//	    
+// 		List<CommentEntity> findComments = commentService.findById(postID);
+// 		
+// 		model.addAttribute("comment", findComments);
+// 		return "post/post";	
+// 		
+// 	}
+	//取得所有貼文
+// 	@GetMapping
+// 	public String findAll(Model model) {
+// 		List<PostEntity> posts = postService.findAll();
+// 		model.addAttribute("posts",posts);
+// 		
+// 		return "post/PostList";	
+//		
+//	}
 }
