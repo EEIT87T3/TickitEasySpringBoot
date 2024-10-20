c4b463b798738fbd77eb9d7242a3efb95918c334
Liang <pig580@yahoo.com.tw>
2024-10-20 21:52:47 +0800

    前台首頁+單筆商品+購物車頁面

---
+++ src/main/java/com/eeit87t3/tickiteasy/product/controller/AdminProductPageController.java
@@ -0,0 +1,61 @@
+package com.eeit87t3.tickiteasy.product.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.eeit87t3.tickiteasy.product.entity.ProductEntity;
+import com.eeit87t3.tickiteasy.product.entity.ProductPhotoEntity;
+import com.eeit87t3.tickiteasy.product.service.ProductPhotoService;
+import com.eeit87t3.tickiteasy.product.service.ProductService;
+
+
+@Controller
+@RequestMapping("/admin/product")
+public class AdminProductPageController {
+	
+	@Autowired
+	ProductService productService;
+	
+	@Autowired
+	private ProductPhotoService productPhotoService;
+	
+	// 查詢所有商品頁面
+    @GetMapping
+    public String findAllProducts() {
+        return "product/findAllProducts";
+    }
+    
+    // 顯示新增商品頁面
+    @GetMapping("/create")
+    public String createProduct(Model model) {
+        model.addAttribute("categoryList", productService.getProductCategories());
+        model.addAttribute("tagList", productService.getProductTags());
+        return "product/createProduct";
+    }
+    
+    // 修改商品頁面
+    @GetMapping("/{productID}/edit")
+    public String editProduct(@PathVariable("productID") int productID, Model model) {
+        ProductEntity product = productService.findProductById(productID);
+        if (product != null) {
+            model.addAttribute("product", product);
+            model.addAttribute("categories", productService.getProductCategories());
+            model.addAttribute("tags", productService.getProductTags());
+            
+            // 獲取並添加商品詳細圖片
+            List<ProductPhotoEntity> productPhotos = productPhotoService.getProductPhotos(product);
+            model.addAttribute("productPhotos", productPhotos);
+            
+            return "product/editProduct";
+        } else {
+            return "redirect:/admin/product";
+        }
+    }
+	
+}
