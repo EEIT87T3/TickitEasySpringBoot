0b6c18e2befc14ee756b0b38e1d47b5ae8be5852
chuan13 <chuan02421@gmail.com>
2024-10-22 21:05:35 +0800

    feat: 查詢單筆活動架構完成、活動票券購物車完成。

--- src/main/java/com/eeit87t3/tickiteasy/event/service/UserEventsService.java
+++ src/main/java/com/eeit87t3/tickiteasy/event/service/UserEventsService.java
@@ -5,7 +5,9 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.eeit87t3.tickiteasy.event.dto.EventWithTicketTypesDTO;
 import com.eeit87t3.tickiteasy.event.dto.EventsSearchingDTO;
 import com.eeit87t3.tickiteasy.event.entity.EventsEntity;
 
@@ -18,7 +20,12 @@ public class UserEventsService {
 	
 	public Page<EventsEntity> findBySpecification(EventsSearchingDTO eventsSearchingDTO) {
 		eventsSearchingDTO.setStatuses(Arrays.asList((short) 1, (short) 2));  // 前台只能查詢到已上架的活動
+		eventsSearchingDTO.setPageSize(12);  // 前台的單頁筆數固定為 12
 		return eventsProcessingService.findBySpecification(eventsSearchingDTO);
+	}
+	
+	public EventWithTicketTypesDTO findById(Integer eventID) {
+		return new EventWithTicketTypesDTO(eventsProcessingService.findById(eventID));
 	}
 	
 }
