8b83336f343492f464d8aa957383fe62867c3b0c
tony475767 <76587204+tony475767@users.noreply.github.com>
2024-10-25 18:17:24 +0800

    前台訂單頁面、前台訂單詳細頁面

---
+++ src/main/java/com/eeit87t3/tickiteasy/order/entity/ClientSideOrderDetails.java
@@ -0,0 +1,36 @@
+package com.eeit87t3.tickiteasy.order.entity;
+
+import com.eeit87t3.tickiteasy.event.entity.TicketTypesEntity;
+import com.eeit87t3.tickiteasy.product.entity.ProductEntity;
+
+public class ClientSideOrderDetails {
+	private Integer price;
+	private Integer quantity;
+	private ProductEntity productById;
+	private TicketTypesEntity ticketTypeById;
+	
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	public ProductEntity getProductById() {
+		return productById;
+	}
+	public void setProductById(ProductEntity productById) {
+		this.productById = productById;
+	}
+	public TicketTypesEntity getTicketTypeById() {
+		return ticketTypeById;
+	}
+	public void setTicketTypeById(TicketTypesEntity ticketTypeById) {
+		this.ticketTypeById = ticketTypeById;
+	}
+}
