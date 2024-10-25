702f1f09b93cfc67ce5ac9d785dfe6e2eb389309
Liang <pig580@yahoo.com.tw>
2024-10-25 21:09:11 +0800

    10/25更新

--- src/main/java/com/eeit87t3/tickiteasy/product/repository/ProdFavoritesRepo.java
+++ src/main/java/com/eeit87t3/tickiteasy/product/repository/ProdFavoritesRepo.java
@@ -4,19 +4,31 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.eeit87t3.tickiteasy.product.entity.ProdFavoritesEntity;
 import com.eeit87t3.tickiteasy.product.entity.ProdFavoritesEntity.ProdFavoriteID;
+import com.eeit87t3.tickiteasy.product.entity.ProductEntity;
+
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import jakarta.transaction.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProdFavoritesRepo extends JpaRepository<ProdFavoritesEntity, ProdFavoriteID> {
 	
-	// 查詢特定會員的特定商品收藏狀態
+	 // 查詢需要發送補貨通知的收藏記錄
+    List<ProdFavoritesEntity> findByProductProductIDAndNotifyStatus(Integer productID, Integer notifyStatus);
+    
+	
+	// 查詢特定會員的特定商品收藏狀態(檢查會員是否已收藏此商品，以決定顯示「收藏」還是「取消收藏」按鈕)
     @Query("SELECT f FROM ProdFavoritesEntity f WHERE f.member.memberID = :memberID AND f.product.productID = :productID")
     Optional<ProdFavoritesEntity> findByMemberIDAndProductID(@Param("memberID") Integer memberID, @Param("productID") Integer productID);
+    
+    // 查詢會員的收藏商品(顯示會員的收藏商品列表頁面)
+    @Query("SELECT f.product FROM ProdFavoritesEntity f WHERE f.member.memberID = :memberID AND f.favoriteCount = :favoriteCount")
+    List<ProductEntity> findProductsByMemberAndFavoriteCount(@Param("memberID") Integer memberID, @Param("favoriteCount") Integer favoriteCount);
+    
     
     // 更新收藏狀態
     @Modifying
